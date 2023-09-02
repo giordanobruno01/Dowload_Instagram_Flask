a43c04903f653e5c5d1f8ad717b9cfdf22f69d26
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-09-02 11:12:33 +0100

    instaloader not loader

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from flask import Flask, request, url_for, redirect, render_template 
 import instaloader
 app = Flask(__name__)
-
+ 
 instaL = instaloader.Instaloader()
-@app.route("/", methods =["POST, GET"] )
+@app.route("/", methods =["POST", "GET"] )
 def logInsta():
-    user = instaL.login(request.form.get("username"), request.form.get("password"))
-    return render_template("index.html")
-if(__name__ == "__main__"):
-    app.run(debug=True)
-# export FLASK_APP=Unfollower_Instagram_Flask
-# export FLASK_DEBUG=1
-# FLASK_APP="main.py"
+    
+    if(request.method == "POST"):
+        usern = request.form.get("username").strip()
+        passw = request.form.get("password").strip()
 
+        instaL.login("flask_test", "giordanobruno01")
+
+        user = instaloader.Profile.from_username(instaL.context, "flask_test")
+
+        followers = user.get_followers()
+
+        # instaL.download_profile("flask_test", profile_pic_only=False)
+
+        
+    
+        return render_template("index.html", name = followers)
+    else: 
+        return render_template("index.html")
+# source auth/bin/activate
+# export FLASK_APP=Unfollower_Instagram_Flask
+# FLASK_APP="main.py"
+# export FLASK_DEBUG=1
+ 
+
+ 
