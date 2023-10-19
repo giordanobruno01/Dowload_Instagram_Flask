936ba79796bb8cf26e9ba311cfd4451393dc8aa2
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-10-19 14:02:42 +0100

    block added

--- static/js/script.js
+++ static/js/script.js
@@ -16,30 +16,114 @@
     // Toggle open and close nav styles on click
     $("#nav-toggle").click(function () {
       $("nav ul").slideToggle();
-    }); 
+    });
     // Hamburger to X toggle
     $("#nav-toggle").on("click", function () {
       this.classList.toggle("active");
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
- 
+
 // $(function ($) {
-  
+
 //   $("hash").click(function () {
 //     console.log("hi");
 //     $("#extra").toggle();
 //   });
 // });
 $(document).ready(function () {
-  $("#hash").change(function () { 
+  $("#hash").change(function () {
     if ($(this).val() == "hashtag") {
-      $("#extra").show();
-      $("#amount").show();
-    } else {
-      $("#extra").hide();
-      $("#amount").hide();
-    }
+      $("#hashtag").show();
+      $("#profile").hide();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "profile") {
+      $("#profile").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "stories") {
+      $("#stories").show();
+      $("#profile").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "reels") {
+      $("#reels").show();
+      $("#stories").hide();
+      $("#profile").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "highlights") {
+      $("#highlights").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#profile").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "post") {
+      $("#post").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#profile").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "pictures") {
+      $("#pictures").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#profile").hide();
+      $("#igtv ").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "igtv") {
+      $("#igtv").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#hashtag").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#profile").hide();
+      $("#test").hide();
+    } else if ($(this).val() == "test") {
+      $("#test").show();
+      $("#stories").hide();
+      $("#reels").hide();
+      $("#profile").hide();
+      $("#highlights").hide();
+      $("#post").hide();
+      $("#pictures").hide();
+      $("#igtv ").hide();
+      $("#hashtag").hide();
+    } 
   });
 });
 // $(document).ready(function () {
