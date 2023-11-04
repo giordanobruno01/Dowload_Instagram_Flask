e8883d4714f19450436a0c5d677c7b542146470f
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-11-04 12:13:09 +0000

    fetcg

--- app.py
+++ app.py
@@ -1,122 +1,136 @@
-from flask import Flask, request, url_for, redirect, render_template, send_file, flash 
-import instaloader, shutil, os
+from flask import Flask, request, url_for, redirect, render_template, send_file, flash
+import instaloader
+import shutil
+import os
 import datetime
 
 from instaloader import *
-app = Flask(__name__)   
-app.secret_key = "hellome"  
+app = Flask(__name__)
+app.secret_key = "hellome"
 if __name__ == "__main__":
-    
-    app.run(debug=True) 
-    #port=8080
-  
+
+    app.run(debug=True)
+    # port=8080
+
 instaL = instaloader.Instaloader()
-def deletezip(): 
+
+
+def deletezip():
     try:
-        for i in os.listdir("."):   
+        for i in os.listdir("."):
             if i.endswith(".zip"):
                 os.remove(i)
     except:
         pass
 
-@app.route("/", methods =["POST", "GET"] )
+
+@app.route("/", methods=["POST", "GET"])
 def logInsta():
-    
-    if(request.method == "POST"):
-        try:
-            usern = request.form.get("username").strip()
-            if(request.form.get("opt").strip()=="hashtag"):
-                hashtag = request.form.get("extra")
-                amount = int(request.form.get("amount"))
-            # passw = request.form.get("password").strip()
-        except:
-            
-            return render_template("index.html", name = "Provide a username")
-        
-        opt = request.form.get("opt")
+
+    if (request.method == "POST"):
+
+        usern = request.form.get("username").strip()
+        # opt = request.form.get("opt")
+        if (usern == ""):
+            return render_template("index.html", name="Provide a username")
+        # passw = request.form.get("password").strip()
+
         deletezip()
-       
+
         try:
-            if(opt =="stories"):
+            opt = request.form.get("opt")
+            if (opt == "stories"):
                 # instaL.login(user = usern, passwd=passw)
                 # instaL.download_stories(userids = [instaL.check_profile_id(usern)])
                 instaL.download_storyitem()
-            elif(opt =="profile"):
+            elif (opt == "profile"):
                 
-                instaL.download_profile(usern, profile_pic_only=True, download_stories=True)
+                
+                try:
+                    instaL.download_profile(
+                    usern, profile_pic_only=True, download_stories=True)
+                except:
+                    return render_template("index.html", name=str(usern + " Try inside profile " + opt))
                 shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
-               
-            
-            elif(opt=="reels"):
+
+            elif (opt == "reels"):
 
                 instaL.download_videos()
-                shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
-                    
-            elif(opt=="hashtag"):  
-                
+                shutil.make_archive(
+                    base_name=usern, format="zip", root_dir=usern)
+
+            elif (opt == "hashtag"):
+                hashtag = request.form.get("extra")
+                amount = int(request.form.get("amount"))
+
                 instaL.download_hashtag(hashtag, max_count=amount)
-                shutil.make_archive(base_name=hashtag, format="zip", root_dir=str("#"+hashtag))
+                shutil.make_archive(base_name=hashtag,
+                                    format="zip", root_dir=str("#"+hashtag))
                 try:
                     shutil.rmtree(str("#"+hashtag))
                 except:
                     pass
-                return send_file(str(hashtag+".zip"),as_attachment=True)
-                
-                
-            elif(opt=="highlights"):
+                return send_file(str(hashtag+".zip"), as_attachment=True)
+
+            elif (opt == "highlights"):
                 instaL.download_highlights()
-                shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
-                
-            elif(opt=="post"):
-                
+                shutil.make_archive(
+                    base_name=usern, format="zip", root_dir=usern)
+
+            elif (opt == "post"):
+
                 prof = Profile.from_username(instaL.context, usern)
 
                 post = prof.get_posts()
-                
+
                 for i in post:
-                    instaL.download_post(i,target=usern) 
-                
-                return send_file(shutil.make_archive(base_name=usern, format="zip", root_dir=usern),as_attachment=True)
+                    instaL.download_post(i, target=usern)
+
+                return send_file(shutil.make_archive(base_name=usern, format="zip", root_dir=usern), as_attachment=True)
                 # instaL.download_post(target="https://www.instagram.com/p/Cwr2kd0tl2q/?utm_source=ig_web_button_share_sheet")
-                    
-            elif(opt=="pictures"):
+
+            elif (opt == "pictures"):
 
                 instaL.download_pictures()
-                shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
-                
-            elif(opt=="igtv"):
+                shutil.make_archive(
+                    base_name=usern, format="zip", root_dir=usern)
+
+            elif (opt == "igtv"):
 
                 instaL.download_igtv()
-                shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
+                shutil.make_archive(
+                    base_name=usern, format="zip", root_dir=usern)
 
-            elif(opt=="test"):
+            elif (opt == "test"):
                 startdate = request.form.get("startdate").split("-")
                 enddate = request.form.get("startdate").split("-")
                 f = startdate.split()[0]
-                instaL.download_profile(usern, post_filter = lambda post: (post.date_utc >= datetime.datetime(int(startdate[0]), int(startdate[1]), int(startdate[2])) and post.date_utc <= datetime.datetime(int(enddate[0]), int(enddate[1]), int(enddate[2])))) 
+                instaL.download_profile(usern, post_filter=lambda post: (post.date_utc >= datetime.datetime(int(startdate[0]), int(
+                    startdate[1]), int(startdate[2])) and post.date_utc <= datetime.datetime(int(enddate[0]), int(enddate[1]), int(enddate[2]))))
                 # shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
-        
-        except: 
-            
-            return render_template("index.html", name = str(usern + " is a private account"))
-        
+
+        except:
+
+            return render_template("index.html", name=str(usern + " is a private account"))
+
         shutil.make_archive(base_name=usern, format="zip", root_dir=usern)
         try:
-            shutil.rmtree(usern)  
+            shutil.rmtree(usern)
         except:
             pass
-    
+
         # instaL.download_feed_posts(max_count=2)
 
-        return send_file(str(usern+".zip"),as_attachment=True)
-    else: 
+        return send_file(str(usern+".zip"), as_attachment=True)
+    else:
         return render_template("index.html")
-      
+
+# python3 -m venv auth
 # source auth/bin/activate
+# pip3 install gunicorn
+# pip3 install requirements.txt
 # export FLASK_APP=Unfollower_Instagram_Flask
 # FLASK_APP="app.py"
-# export FLASK_DEBUG=1 
-#gunicorn --bind 0.0.0.0:5000 app:app --daemon
-  
-
- 
+# export FLASK_DEBUG=1
+# gunicorn --bind 0.0.0.0:5000 app:app --daemon
+# pkill gunicorn
